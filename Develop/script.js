4ec57cd2c29c81a10207bbfa0e92cd79b3bde3a9
Obrienjames34 <Obrienjames934@gmail.com>
2022-07-20 14:58:56 -0400

    added character code

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+//Characters code
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMONPQRSTUVWXYZ';
+var numbers ='123456789';
+var specialCharacters = '!@#$%^&*';
+var storePassword = 'generate password';
+var passwordlength = 0;
+var password = "";
+var allCharacters = "";
+
+//
+
+
+// Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
