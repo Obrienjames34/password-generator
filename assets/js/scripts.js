9c7a531cd7e869ef51eb35583669035f245c6da7
Obrienjames34 <Obrienjames934@gmail.com>
2022-08-10 09:50:04 -0400

    added password

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,80 @@
+// Characters code
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '123456789';
+var specialCharacters = '!@#$%^&*_-+=';
+var storePassword = "generated password";
+var passLength = 0;
+var password = "";
+var allCharacters = "";
+
+// create password
+var createPass = function () {
+  // reset characters, password & length
+  allCharacters = "";
+  password = "";
+  passLength = 0;
+  // ensure code is between 8-128 characters
+  while (passLength < 8 || passLength > 128) {
+    passLength = window.prompt(
+      "Please type a number between 8 and 128 for your password length"
+    );
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
