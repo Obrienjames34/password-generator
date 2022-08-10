6c2a7e6bd723b3c70c159deaf22b0dbccee752b0
Obrienjames34 <Obrienjames934@gmail.com>
2022-08-10 13:00:47 -0400

    code has characters 8-128

--- Develop/script.js
+++ Develop/script.js
@@ -8,8 +8,20 @@ var passwordlength = 0;
 var password = "";
 var allCharacters = "";
 
-//
+// create password
+var createPass = function () {
+  // reset characters, password & length
+  allCharacters = "";
+  password = "";
+  passLength = 0;
+}
 
+// ensure code is between 8-128 characters
+while (passLength < 8 || passLength > 128) {
+  passLength = window.prompt(
+    "Please type a number between 8 and 128 for your password length"
+  );
+  }
 
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
